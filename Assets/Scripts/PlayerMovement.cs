d486e028b13e5535a6c53fa9c150901aea554e39
Hnthpcool6 <hnt.vn.vn@gmail.com>
2024-02-28 09:32:50 +0700

    ud Player Animator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D coll;
     private SpriteRenderer sprite;
+    private Animator anim;
+
 
     [SerializeField] private LayerMask jumpableGround;
     private float dirX = 0f;
@@ -18,17 +20,34 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
     }
 
     void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-
         dirY = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector2(rb.velocity.x, dirY * moveSpeed);
-            
-   
+        updateAnim();
+    }
+
+    private void updateAnim(){
+        if (dirX > 0f)
+        {
+            sprite.flipX = false;
+            anim.Play("run");
+
+        }
+        else if (dirX < 0f)
+        {
+            sprite.flipX = true;
+            anim.Play("run");
+        }    
+        else{
+            anim.Play("idle");
+        }
     }
 
 }

e41c0b8d5533ea369597f1fa66a20b6af331fcec
Hnthpcool6 <hnt.vn.vn@gmail.com>
2024-03-04 01:50:08 +0700

    ud tilemap switcher

--- Assets/Scripts/GameTilemapSwitcher.cs
+++ Assets/Scripts/GameTilemapSwitcher.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameTilemapSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] public float switchInterval;
+    public Tilemap[] tilemaps;
+
+    private Tilemap currentTilemap;
+    private int currentTilemapIndex = 0;
+    private float timer;
+
     void Start()
     {
-        
+        timer = switchInterval;
+        SwitchTilemap();
+        currentTilemap.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            SwitchTilemap();
+            timer = switchInterval;
+        }
+    }
+
+    void SwitchTilemap()
+    {
+        if (tilemaps.Length > 0)
+        {
+            if (currentTilemap != null)
+            {
+                currentTilemap.gameObject.SetActive(false);
+            }
+
+            currentTilemapIndex = (currentTilemapIndex + 1) % tilemaps.Length;
+            currentTilemap = tilemaps[currentTilemapIndex];
+            currentTilemap.gameObject.SetActive(true);
+        }
     }
 }
